3e5473620bed9ab2a317037ab744acad901e44e7
junin <juninho.chandra@my.jcu.edu.au>
2025-02-09 15:32:58 +0800

    Refactored score program to use 1 functions to return a message prompt.

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,18 +1,24 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
+import random
 
 PROMPT = "Enter score: "
 
-score = float(input(PROMPT))
-while score < 0 or score > 100:
-    print("Invalid score")
+def main():
     score = float(input(PROMPT))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input(PROMPT))
+    print(get_result(score))
+    # I don't quite get what the question is asking, maybe this?
+    score = random.randint(0, 100)
+    print(score)
 
-if  score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def get_result(score):
+    # returns a prompt message depending on the score.
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()

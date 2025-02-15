df5bfe76a07df95d66d8c293a51783303f34969d
junin <juninho.chandra@my.jcu.edu.au>
2025-02-15 23:07:03 +0700

    created randoms.py, answer the repo questions and write a code involving random module.

---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+# Question 1
+# I see that it prints a random integer between 5 and 20, inclusive.
+# I see 8 as the smallest and 19 as the largest.
+
+# Question 2
+# I see that it prints a random odd integer between 3 and 10, inclusive.
+# I see 3 as the smallest and 7 as the largest.
+# Line 2 could have not produce a 4.
+
+# Question 3
+# I see that it prints a random decimal number between 2.5 and 5.5, inclusive.
+# I see 2.695104866405516 as the smallest and 5.025159319290196 as the largest.
+
+import random
+
+print(random.uniform(0, 100))

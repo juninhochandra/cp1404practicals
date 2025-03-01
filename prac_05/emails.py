210e5aef6071ceecdbd97f55013ce2e3b23ea01d
junin <juninho.chandra@my.jcu.edu.au>
2025-03-02 05:03:44 +0800

    created emails.py and did all the repo steps.

---
+++ prac_05/emails.py
@@ -0,0 +1,22 @@
+"""
+Word Occurrences
+Estimate: 25 minutes
+Actual:   22 minutes
+"""
+
+name_to_email = dict()
+email = input("Email: ")
+
+# Should we check if the email is invalid?
+while email != "":
+    full_name = email.split("@")[0].split(".")
+    full_name = " ".join(full_name).title()
+    name_confirm = input(f"Is your name {full_name}? (Y/n) ").upper()
+    if name_confirm != "" and name_confirm != "Y":
+        full_name = input("Name: ")
+    name_to_email[full_name] = email
+    email = input("Email: ")
+
+print()
+for name, email in name_to_email.items():
+    print(f"{name} ({email})")

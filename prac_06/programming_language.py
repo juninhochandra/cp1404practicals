99e07ef3fac3c96ccdb06d0dfaadba801f39ba80
junin <juninho.chandra@my.jcu.edu.au>
2025-03-08 21:29:40 +0800

    added programming_language.py and languages.py and did all repo steps.

---
+++ prac_06/programming_language.py
@@ -0,0 +1,20 @@
+"""
+Emails
+Estimate: 7 minutes
+Actual:   7 minutes
+"""
+
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
+        """Constructor"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Returns true if the language is dynamically typed"""
+        return self.typing == "Dynamic"
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"

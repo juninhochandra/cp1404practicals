3fc4ddcc5e02dfe29a49739d49b8fd755271cd87
junin <juninho.chandra@my.jcu.edu.au>
2025-03-23 19:53:48 +0800

    Refactored code.

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -5,23 +5,15 @@ from guitar import Guitar
 def main():
     """Read file of guitar details, save as objects, display."""
     guitars = []
-    # Open the file for reading
-    in_file = open('guitars.csv', 'r')
-    for line in in_file:
-        parts = line.strip().split(',')
-        guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
-        guitars.append(guitar)
-    # Close the file as soon as we've finished reading it
-    in_file.close()
-
-    add_guitar(guitars)
+    read_guitars(guitars)
+    add_guitars(guitars)
 
     guitars.sort()
     # Loop through and display all guitars (using their str method)
     for guitar in guitars:
         print(guitar)
 
-def add_guitar(guitars):
+def add_guitars(guitars):
     in_file = open('guitars.csv', 'a')
     guitar_name = input("Name: ")
     while guitar_name != "":
@@ -37,6 +29,16 @@ def add_guitar(guitars):
         print(f"{guitar_name:} ({guitar_year}) : ${guitar_cost} added.")
         print()
         guitar_name = input("Name: ")
+    in_file.close()
+
+def read_guitars(guitars):
+    """Open the file for reading and store in a list"""
+    in_file = open('guitars.csv', 'r')
+    for line in in_file:
+        parts = line.strip().split(',')
+        guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
+        guitars.append(guitar)
+    # Close the file as soon as we've finished reading it
     in_file.close()
 
 main()

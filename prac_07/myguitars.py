2f8745432b34fb1f23cd118ea2d26ebd8e348c5e
junin <juninho.chandra@my.jcu.edu.au>
2025-03-23 19:50:40 +0800

    added add_guitar function for adding guitar.

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -14,12 +14,29 @@ def main():
     # Close the file as soon as we've finished reading it
     in_file.close()
 
-    guitars.sort()
+    add_guitar(guitars)
 
+    guitars.sort()
     # Loop through and display all guitars (using their str method)
     for guitar in guitars:
         print(guitar)
 
+def add_guitar(guitars):
+    in_file = open('guitars.csv', 'a')
+    guitar_name = input("Name: ")
+    while guitar_name != "":
+        """Input year and cost"""
+        guitar_year = int(input("Year: "))
+        guitar_cost = float(input("Cost: $"))
 
+        """Create guitar object and append to guitars"""
+        guitar = Guitar(guitar_name, guitar_year, guitar_cost)
+
+        guitars.append(guitar)
+        in_file.write(f"\n{guitar_name},{guitar_year},{guitar_cost}")
+        print(f"{guitar_name:} ({guitar_year}) : ${guitar_cost} added.")
+        print()
+        guitar_name = input("Name: ")
+    in_file.close()
 
 main()

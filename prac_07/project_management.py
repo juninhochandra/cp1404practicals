2745e5d580538d24f1029ff3687a5b7494963dcb
junin <juninho.chandra@my.jcu.edu.au>
2025-03-23 22:17:39 +0800

    Added add_project function for adding a new project, refactored getting percentage and priority from user.

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -9,8 +9,6 @@ from project import Project
 
 FILE_NAME = "projects.txt"
 PROJECT_CHOICE = "Project choice: "
-PERCENTAGE_CHOICE = "New percentage: "
-PRIORITY_CHOICE = "New priority: "
 MENU = """- (L)oad projects
 - (S)ave projects
 - (D)isplay projects
@@ -40,7 +38,7 @@ def main():
         elif choice == "f":
             pass
         elif choice == "a":
-            pass
+            add_project(projects)
         elif choice == "u":
             update_project(projects)
         else:
@@ -93,20 +91,49 @@ def update_project(projects):
     project = projects[project_choice]
     print(project)
 
-    is_valid_choice = False
-    percentage_choice = input(PERCENTAGE_CHOICE)
-    if percentage_choice == "":
-        is_valid_choice = True
-    while not is_valid_choice:
-        try:
-            percentage_choice = int(percentage_choice)
-            if percentage_choice > -1 or percentage_choice < 101:
-                is_valid_choice = True
-        except ValueError:
-            percentage_choice = input(PERCENTAGE_CHOICE)
+    priority_choice = get_priority("New priority: ")
+    percentage_choice = get_percentage("New percentage: ")
+
+    if percentage_choice != "":
+        setattr(project, "completion", percentage_choice)
+    if priority_choice != "":
+        setattr(project, "priority", priority_choice)
+
+def add_project(projects):
+    print("Let's add a new project")
+    project_name = input("Name: ")
 
     is_valid_choice = False
-    priority_choice = input(PRIORITY_CHOICE)
+    project_date = input("Start date (dd/mm/yy): ")
+    while not is_valid_choice:
+        try:
+            is_valid_choice = bool(datetime.datetime.strptime(project_date, "%d/%m/%Y"))
+        except ValueError:
+            project_date = input("Start date (dd/mm/yy): ")
+
+    project_priority = get_priority("Priority: ")
+    while project_priority == "":
+        project_priority = get_priority("Priority: ")
+
+    is_valid_choice = False
+    project_cost = input("Cost estimate: $")
+    while not is_valid_choice:
+        try:
+            project_cost = float(project_cost)
+            is_valid_choice = True
+        except ValueError:
+            project_cost = input("Cost estimate: $")
+
+    project_completion = get_percentage("Percent complete: ")
+    while project_completion == "":
+        project_completion = get_percentage("Percent complete: ")
+
+    project = Project(project_name, project_date, project_priority, project_cost, project_completion)
+    projects.append(project)
+
+def get_priority(prompt):
+    is_valid_choice = False
+    priority_choice = input(prompt)
     if priority_choice == "":
         is_valid_choice = True
     while not is_valid_choice:
@@ -114,12 +141,26 @@ def update_project(projects):
             priority_choice = int(priority_choice)
             if priority_choice > 0:
                 is_valid_choice = True
+            else:
+                priority_choice = input(prompt)
         except ValueError:
-            priority_choice = input(PRIORITY_CHOICE)
+            priority_choice = input(prompt)
+    return priority_choice
 
-    if percentage_choice != "":
-        setattr(project, "completion", percentage_choice)
-    if priority_choice != "":
-        setattr(project, "priority", priority_choice)
+def get_percentage(prompt):
+    is_valid_choice = False
+    percentage_choice = input(prompt)
+    if percentage_choice == "":
+        is_valid_choice = True
+    while not is_valid_choice:
+        try:
+            percentage_choice = int(percentage_choice)
+            if -1 < percentage_choice < 101:
+                is_valid_choice = True
+            else:
+                percentage_choice = input(prompt)
+        except ValueError:
+            percentage_choice = input(prompt)
+    return percentage_choice
 
 main()

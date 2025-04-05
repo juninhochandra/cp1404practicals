5a6cf98f784a195f994031c6c9f3bc53bf41bc2b
junin <juninho.chandra@my.jcu.edu.au>
2025-04-06 02:05:05 +0800

    Deleted 1 method for SilverServiceTaxi class.

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -10,8 +10,5 @@ class SilverServiceTaxi(Taxi):
     def get_fare(self):
         return self.flagfall + self.price_per_km * self.current_fare_distance
 
-    def start_fare(self):
-        self.current_fare_distance = self.flagfall
-
     def __str__(self):
         return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
